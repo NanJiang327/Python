4474bf4e181d33156bf1af956d78be0c40d54d17
NanJiang327 <nj499521010@gmail.com>
2019-06-23 19:42:53 +1200

    Added local settings import

--- property-site/btre/settings.py
+++ property-site/btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aaronj1.9403@gmail.com'
 EMAIL_HOST_PASSWORD = '123.comwyxx'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
